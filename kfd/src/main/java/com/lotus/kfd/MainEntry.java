45bd7ff81b6a4cc66d59985154a0f9ea70968f71
Guo chen <chen.guo@flextrade.com>
2017-12-26 21:17:43 -0500

    update

--- kfd/src/main/java/com/lotus/kfd/MainEntry.java
+++ kfd/src/main/java/com/lotus/kfd/MainEntry.java
@@ -2,19 +2,27 @@ package com.lotus.kfd;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 public class MainEntry {
 
     public static void main(String... args) {
-        Vertx.vertx(new VertxOptions())
-                .createHttpServer()
-                .requestHandler(httpServerRequest -> {
-            httpServerRequest.response()
-                    .putHeader("content-length", String.valueOf("success".length()))
-                    .putHeader("content-type", "text/html")
-                    .setStatusCode(200)
-                    .write("success")
-                    .end();
-        }).listen(9090);
+        ClusterManager mgr = new HazelcastClusterManager();
+        Vertx.clusteredVertx(new VertxOptions()
+                .setWorkerPoolSize(40)
+                .setClusterManager(mgr), clusterRes -> {
+                    if (clusterRes.succeeded()) {
+                        Vertx vertx = clusterRes.result();
+                        vertx.createHttpServer()
+                                .requestHandler(httpServerRequest ->
+                                        httpServerRequest.response()
+                                        .putHeader("content-length", String.valueOf("success".length()))
+                                        .putHeader("content-type", "text/html")
+                                        .setStatusCode(200)
+                                        .write("success")
+                                        .end()).listen(9090);
+                    }
+        });
     }
 }
